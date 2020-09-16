35c3bff9b9c17598309c08fe1c0fb13c50342da8
Daniel Zurita <danielzurita94@hotmail.com>
2020-09-16 17:54:09 -0500

    globales

---
+++ modulos/globales.js
@@ -0,0 +1,21 @@
+// let i = 0;
+
+// let intervalo = setInterval(function () {
+//   console.log('Hola');
+//   if (i === 3) {
+//     clearInterval(intervalo);
+//   }
+//   i++;
+// });
+
+
+// Se ejecuta inmediado
+// setImmediate(function () {
+//   console.log('Hola');
+// });
+
+// console.log(process);
+// console.log(__dirname);
+// console.log(__filename);
+global.miVariable = 'elValor';
+console.log(miVariable);
